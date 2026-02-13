58b01f9448ed9d1bd734836f4865087bbee58c52
Faceless3882 <148793046+Faceless3882@users.noreply.github.com>
2026-02-13 21:55:31 +0800

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -5,14 +5,20 @@ echo "(This might turn off your TV)"
 sleep 3
 sudo systemctl disable LG_Buddy.service
 sudo systemctl disable LG_Buddy_wake.service
+sudo systemctl disable LG_Buddy_screen.service
 sudo systemctl stop LG_Buddy.service
 sudo systemctl stop LG_Buddy_wake.service
+sudo systemctl stop LG_Buddy_screen.service
 sudo rm /etc/systemd/system/LG_Buddy.service
 sudo rm /etc/systemd/system/LG_Buddy_wake.service
+sudo rm /etc/systemd/system/LG_Buddy_screen.service
 echo "Done."
 
-echo "Removing Startup, Shutdown, and Sleep scripts"
+echo "Removing scripts"
 sudo rm /usr/bin/LG_Buddy_Startup
 sudo rm /usr/bin/LG_Buddy_Shutdown
+sudo rm /usr/bin/LG_Buddy_Screen_On
+sudo rm /usr/bin/LG_Buddy_Screen_Off
+sudo rm /usr/bin/LG_Buddy_Screen_Monitor
 sudo rm /etc/NetworkManager/dispatcher.d/pre-down.d/LG_Buddy_sleep
 echo "Done."
